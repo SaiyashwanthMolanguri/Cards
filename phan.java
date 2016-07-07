ce1382a3f36c994e8477641b6fdb43995ef4f642
shashank <desertsandocean@gmail.com>
2016-07-07 15:45:43 +0530

    up1

--- phan.java
+++ phan.java
@@ -2,18 +2,19 @@ class Cards{
 
 	String pip;
 	int cardNum;
-	int suit;
-	static String[]  cardSuit = new String[4];
-	static String[]  cardNumber = new cardNumber[13];
+	String suit;
+	static String[] cardSuit = new String[4];
+	static String[] cardNumber = new String[13];
 	
 	static{
+		
 		cardSuit[0] = "CLUBS";
 		cardSuit[1] ="DIAMOND";
 		cardSuit[2] ="HEART";
 		cardSuit[3] = "SPADES";
 		
 		for(int i=0;i<13;++i)
-			cardNumber[i]= (i+1).toString();
+			cardNumber[i]= Integer.toString(i+1);
 		
 		cardNumber[0]="A";
 		cardNumber[10]= "J";
@@ -25,7 +26,7 @@ class Cards{
 	Cards(int n){
 		
 		cardNum = n/13+1;
-		pip= cardNumber[n%13]
+		pip= cardNumber[n%13];
 		suit = cardSuit[n/13];
 		
 	}
@@ -39,7 +40,7 @@ class Cards{
 	}
 		
 	String getSuit(){
-		return cardSuit[pip];
+		return suit;
 	}
 
 
