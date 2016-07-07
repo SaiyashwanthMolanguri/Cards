1ef84a993456a9560951f43a7313964e9c3279ab
shashank <desertsandocean@gmail.com>
2016-07-07 15:25:36 +0530

    editr 1

--- Cards.java
+++ Cards.java
@@ -1,6 +1,80 @@
-public class Cards{
+import java.util.*;
+
+class Cards{
 
 	int pip;
 	String suit;
 	
+	Cards(int pip , String suit){
+		this.pip = pip;
+		this.suit = suit;
+	}
+	
+	void string toString(){
+		System.out.println("This is "+ pip+" of "+suit);
+	
+	}
+
 }
+
+class Deck implements Comparator{
+	
+	int noOfCardsLeft;
+	ArrayList<Cards> deck;
+	
+	Deck(String suit){
+		noOfCardsLeft = 13;
+		deck = new ArrayList<Cards>();
+		
+		for(int i=1; i<=13;++i)
+			deck.add(i,suit); 
+	
+	}
+
+	void drawACard(Card c){
+		deck.remove(c); 
+	}
+	
+	void sortCards(){
+		Collections.sort(deck);
+	}
+	
+	public int compare(Cards c1, Cards c2){
+		return (c1.pip- c2.pip);
+	}
+	
+}
+
+class DiamondGame{
+	
+	String name;
+	String playerSuit;
+	String computerSuit;
+	int playerTurn;
+	int computerTurn;
+	boolean 
+	DiamondGame(String name){
+		this.name = name;
+		begin();
+	}			
+	
+	void begin(){
+		Random rand;
+		int randomNum = rand.nextInt((2);
+		
+		playerTurn = randomNum;
+		computerTurn= (randomNum+1)%2;
+		
+		while(gameContinue){
+		
+		}
+		
+		
+	}
+	
+	
+
+}	
+	
+	
+
--- phan.java
+++ phan.java
@@ -3,10 +3,10 @@ class Cards{
 	String pip;
 	int cardNum;
 	int suit;
-	String[]  cardSuit = new String[4];
-	String[]  cardNumber = new cardNumber[13];
-	Cards(int n){
-		
+	static String[]  cardSuit = new String[4];
+	static String[]  cardNumber = new cardNumber[13];
+	
+	static{
 		cardSuit[0] = "CLUBS";
 		cardSuit[1] ="DIAMOND";
 		cardSuit[2] ="HEART";
@@ -19,6 +19,10 @@ class Cards{
 		cardNumber[10]= "J";
 		cardNumber[11]="Q";	
 		cardNumber[12]="K";
+	
+	}
+	
+	Cards(int n){
 		
 		cardNum = n/13+1;
 		pip= cardNumber[n%13]
