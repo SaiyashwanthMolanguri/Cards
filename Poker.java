ffb3bf7b06189e307654fa24674c50f284fa68a9
phanindra <phanindra.venganti2@gmail.com>
2016-07-07 23:20:48 +0530

    Added threeofakind and Twopair Conditions

--- Poker.java
+++ Poker.java
@@ -1,13 +1,13 @@
 public static boolean flush (String h1) {
-for (int i=0;i<4;i++) {
+	for (int i=0;i<4;i++) {
 	if (Cards[i].getsuit()! = Cards[i+1].getSuit() )
 		return false;
 	}
 }
 public static boolean fourofakind (String h1) {
-int count=0;
-int i=0;
-while (int i<4) {
+	int count=0;
+	int i=0;
+	while (int i<4) {
 	if (Cards[x].getpip() = Cards[X+1].getpip() ){
 	count++;
 	i++;
@@ -23,4 +23,47 @@ public static boolean straightflush (string h1) {
 		return false;
 	}
 }
+public static boolean royalflush (string h1) {
+	for(int i=0;i<4;i++) { 
+	if(Cards[i].getsuit()= Cards[i+1].getsuit() && Cards[i].getpip()  > Cards[i+1].getpip() )
+		return false;
+	}
+}
+
+public static boolean straight (string h1) {
+	for(int i=0;i<4;i++) { 
+	if(Cards[i].getpip() < Cards[i+1].getpip() )
+		return false;
+	}
+public static boolean threeofakind (String h1) {
+	int count=0;
+	int i=0;
+	while (int i<4) {
+	if (Cards[i].getpip() = Cards[i+1].getpip() && Cards[i+1] = Cards[i+2] ){
+	count++;
+	i++;
+	}
+	else
+	i++;
+	}
+	if(Count == 1)
+ 	return true;
+}
+public static boolean twopair (String h1) {
+	int count=0;
+	int i=0;
+	while (int i<4) {
+	if (Cards[x].getpip() = Cards[X+1].getpip() ){
+	count++;
+	i++;
+	}
+	else
+	i++;
+	}
+	if(Count == 2)
+ 	return true;
+}
+
+	
+	
 
